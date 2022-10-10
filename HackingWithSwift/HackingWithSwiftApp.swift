8f2d5cb28b56bd16b651a669a9d93226810ab824
AjinVijayan0204 <ajinvijayan0204@gmail.com>
2022-10-10 19:34:24 +0530

    Initial Commit

---
+++ HackingWithSwift/HackingWithSwiftApp.swift
@@ -0,0 +1,20 @@
+//
+//  HackingWithSwiftApp.swift
+//  HackingWithSwift
+//
+//  Created by Ajin on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
